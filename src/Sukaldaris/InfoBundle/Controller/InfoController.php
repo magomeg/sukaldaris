41c8c63a68c87d0953089e0d0518bdfb6c053641
magomeg <magom@opendeusto.es>
2016-10-09 18:15:46 +0200

    Repositories created + receta.html.twig avance (SIN TERMINAR)

--- src/Sukaldaris/InfoBundle/Controller/InfoController.php
+++ src/Sukaldaris/InfoBundle/Controller/InfoController.php
@@ -10,4 +10,12 @@ class InfoController extends Controller
     {
         return $this->render('SukaldarisInfoBundle:Info:index.html.twig');
     }
+    public function exampleAction()
+    {
+    	$receta = $this->get('doctrine')->getManager()->getRepository('SukaldarisInfoBundle:Receta')->find(1);
+
+		$palabras = $this->get('doctrine')->getManager()->getRepository('SukaldarisInfoBundle:PalabraClave')->getPalabrasClaveForReceta(1);
+
+        return $this->render('SukaldarisInfoBundle:Info:receta.html.twig', array('receta' => $receta, 'palabras' => $palabras));
+    }
 }
